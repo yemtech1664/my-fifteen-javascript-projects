4066ec9834a6207dd3e7a6da67be7008937410ac
Tiamiyu Idris <tiamiyuidris2014@gmail.com>
2023-12-20 10:48:28 +0100

    Third commit

--- FAQ/asset/js/script.js
+++ FAQ/asset/js/script.js
@@ -1,13 +1,33 @@
 let questionBtn = document.querySelectorAll(".question-btn")
-let question = document.querySelector(".question");
+let question = document.querySelectorAll(".question");
 
-questionBtn.forEach((btn)=>{
-	//console.log(btn)
-	btn.addEventListener("click", function(e){
-		//console.log(e.currentTarget.parentElement.parentElement.classList);
-		let showQuestion = e.currentTarget.parentElement.parentElement.classList;
-		showQuestion.toggle("show-text")
+question.forEach((item) => {
+	//console.log(item.querySelector(".question-btn"))
+	let btn = item.querySelector(".question-btn");
+	btn.addEventListener("click", function(){
+
+		question.forEach((faq) => {
+			if(faq !== item){
+				faq.classList.remove("show-text");
+			}
+		})
+
+		item.classList.toggle("show-text");
 	})
 })
+
+
+
+
+
+
+// questionBtn.forEach((btn)=>{
+// 	//console.log(btn)
+// 	btn.addEventListener("click", function(e){
+// 		//console.log(e.currentTarget.parentElement.parentElement.classList);
+// 		let showQuestion = e.currentTarget.parentElement.parentElement.classList;
+// 		showQuestion.toggle("show-text")
+// 	})
+// })
 
 //console.log(questionBtn)
